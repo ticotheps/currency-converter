d5381cb312ff2fd38511d607335875e837a1b021
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-16 23:03:35 -0400

    Created 'state' object on <Converter /> component

--- satoshi-converter-app/src/components/Converter.js
+++ satoshi-converter-app/src/components/Converter.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class Converter extends React.Component {
+
+    state = {
+        currencies: ['USD', 'AUD', 'SGD', 'PHP', 'EUR'],
+        base: 'USD',
+        amount: '',
+        convertTo: '',
+        result: '',
+        date: ''
+    }
+
     render() {
         return (
             <div className="card card-body">
@@ -8,7 +18,7 @@ class Converter extends React.Component {
                 <h2>1.87 EUR</h2>
                 <p>As of 1/2/2019</p>
                 <div className="row">
-                    <div className="col-lg-10">
+                    <div className="col-lg-10 col-md-10">
                         <form className="form-inline mb-4">
                             <input className="form-control form-control-lg mx-3"/>                       
                             <select className="form-control form-control-lg">
@@ -22,6 +32,9 @@ class Converter extends React.Component {
                                 <option>Option 1</option>
                             </select>
                         </form>
+                    </div>
+                    <div className="col-lg-2 col-md-2 col-sm-2 align-self-center">
+                        <h1 className="convert-btn">&#8595;&#8593;</h1>
                     </div>
                 </div>
             </div>
