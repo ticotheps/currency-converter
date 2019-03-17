39434c4fc56aa97de7adf7e1d5ecadb2f97d51cb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-16 22:42:05 -0400

    Adds Bootstrap CDN in to <head> element of index.html

--- satoshi-converter-app/src/components/Converter.js
+++ satoshi-converter-app/src/components/Converter.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Converter extends React.Component {
     render() {
         return (
-            <h1>Tico's Satoshi to USD Converter!</h1>
+            <div className="card card-body">
+                <h5>1 USD is equal to</h5>
+                <h2>1.87 EUR</h2>
+                <p>As of 1/2/2019</p>
+                <div>
+                    
+                </div>
+            </div>
         );
     }
 }
