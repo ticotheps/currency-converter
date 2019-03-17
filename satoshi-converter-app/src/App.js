d2aefce24a613b533d7c4010fd27ade05f325797
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-16 22:33:29 -0400

    Creates a new <Converter /> component

--- satoshi-converter-app/src/App.js
+++ satoshi-converter-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Converter from './components/Converter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Satoshi Converter App
+        <Converter />
       </div>
     );
   }
---
+++ satoshi-converter-app/src/components/Converter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Converter extends React.Component {
+    render() {
+        return (
+            <h1>Tico's Satoshi to USD Converter!</h1>
+        );
+    }
+}
+
+export default Converter;
